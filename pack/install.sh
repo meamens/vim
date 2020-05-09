6d282a7edc996cc58346148af639192ca8ecaeca
Marc Eamens <meamens@gmail.com>
2020-05-09 21:45:08 +0100

    Added Terraform formatting

--- pack/install.sh
+++ pack/install.sh
@@ -59,6 +59,7 @@ package https://github.com/martinda/Jenkinsfile-vim-syntax.git &
 package https://github.com/kezhenxu94/vim-mysql-plugin.git &
 package https://github.com/lifepillar/pgsql.vim.git &
 package https://github.com/numirias/semshi.git *
+package https://github.com/hashivim/vim-terraform.git &
 wait
 ) &
 (
